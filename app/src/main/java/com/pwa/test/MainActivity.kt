832a1cb7852d6f83e2a2d8626db7b4486ced8a7d
unknown <feverra@gmail.com>
2020-11-18 01:49:26 +0700

    first commit

---
+++ app/src/main/java/com/pwa/test/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.pwa.test
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    protected var mLastLocation: Location? = null
+    private var mLatitudeLabel: String? = null
+    private var mLongitudeLabel: String? = null
+    private var mLatitudeText: TextView? = null
+    private var mLongitudeText: TextView? = null
+
+    public override fun onStart() {
+        super.onStart()
+        Log.i("MyActivity", "abc1")
+        //getLastLocation()
+        if (!checkPermissions()) {
+            Toast.makeText(this,"Issue with permission",Toast.LENGTH_LONG).show();
+            requestPermissions()
+        } else {
+            getLastLocation()
+        }
+        Log.i("MyActivity", "abc2")
+    }
+
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("MyActivity", "abc3")
+        mLatitudeLabel = resources.getString(R.string.latitude_label)
+        mLongitudeLabel = resources.getString(R.string.longitude_label)
+        mLatitudeText = findViewById<View>(R.id.latitude_text) as TextView
+        mLongitudeText = findViewById<View>(R.id.longitude_text) as TextView
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        mFusedLocationClient!!.lastLocation
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful && task.result != null) {
+                        mLastLocation = task.result
+
+                        mLatitudeText!!.setText(
+                                mLatitudeLabel+":   "+
+                                        (mLastLocation )!!.latitude)
+                        mLongitudeText!!.setText(mLongitudeLabel+":   "+
+                                (mLastLocation )!!.longitude)
+                    } else {
+                        Log.w(TAG, "getLastLocation:exception", task.exception)
+                        showMessage(getString(R.string.no_location_detected))
+                    }
+                }
+    }
+
+    private fun showMessage(text: String) {
+        val container = findViewById<View>(R.id.main_activity_container)
+        if (container != null) {
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(this@MainActivity,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                REQUEST_PERMISSIONS_REQUEST_CODE)
+    }
+    private fun requestPermissions(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        } else {
+            ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.size <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.")
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getLastLocation()
+            } else {
+                // Permission denied.
+
+                // Notify the user via a SnackBar that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                showSnackbar(R.string.permission_denied_explanation, R.string.settings,
+                        View.OnClickListener {
+                            // Build intent that displays the App settings screen.
+                            val intent = Intent()
+//                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts("package",
+                                    BuildConfig.APPLICATION_ID, null)
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        })
+            }
+        }
+    }
+    private fun showSnackbar(mainTextStringId: Int, actionStringId: Int,
+                             listener: View.OnClickListener) {
+        Toast.makeText(this@MainActivity, getString(mainTextStringId), Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        private val TAG = "LocationProvider"
+        private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+feverra/KotlinCurrentLatLng
